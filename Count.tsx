57a53cc95204f32760afc9f2e77118f57e94f6c6
grpartichelli <gabrielpartichelli@gmail.com>
2020-06-08 15:20:41 -0300

    First commit

---
+++ Count.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+
+const styles = StyleSheet.create({
+  text: { fontSize: 72 },
+});
+export const Count = (props) => <Text style={styles.text}>{props.count}</Text>;
